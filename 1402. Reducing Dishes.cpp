c39dffb7273dd156a65d74881ab69947a8e4b5c6
surbhi-kuhar <104093804+surbhi-kuhar@users.noreply.github.com>
2023-08-29 20:59:16 +0530

    Create 1402. Reducing Dishes.cpp

---
+++ 1402. Reducing Dishes.cpp
@@ -0,0 +1,85 @@
+// RECURSION
+
+int solve( vector<int>&satisfaction, int index, int time){
+        if(index == satisfaction.size()){
+            return 0;
+        }
+
+        int include = satisfaction[index] * (time + 1) + solve( satisfaction, index+1, time+1);
+        int exclude = solve( satisfaction, index+1, time);
+
+        return max(include,exclude);
+
+    }
+
+
+// RECURSION + MEMOIZATION 
+
+    int solve( vector<int>&satisfaction, int index, int time, vector<vector<int>>&dp){
+        if(index == satisfaction.size()){
+            return 0;
+        }
+
+        if(dp[index][time]!=-1) return dp[index][time];
+
+        int include = satisfaction[index] * (time + 1) + solve( satisfaction, index+1, time+1,dp);
+        int exclude = solve( satisfaction, index+1, time,dp);
+
+        dp[index][time] = max(include,exclude);
+        return dp[index][time];
+
+    }
+
+// TABULATION
+
+    int solve( vector<int>&satisfaction){
+
+        int n = satisfaction.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int index=n-1;index>=0;index--){
+            for(int time = index; time>=0 ;time--){
+                int include = satisfaction[index] * (time+1) + dp[index+1][time+1];
+                int exclude = dp[index+1][time];
+
+                dp[index][time] = max(include,exclude);
+            }
+        }
+
+        return dp[0][0];
+    }
+
+// SPACE OPTIMIZATION
+
+    int solve( vector<int>&satisfaction){
+
+        int n = satisfaction.size();
+
+        vector<int>next(n+1,0);
+        vector<int>curr(n+1,0);
+
+        for(int index=n-1;index>=0;index--){
+            for(int time = index; time>=0 ;time--){
+                int include = satisfaction[index] * (time+1) + next[time+1];
+                int exclude = next[time];
+
+                curr[time] = max(include,exclude);
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
+
+    int maxSatisfaction(vector<int>& satisfaction) {
+
+        sort(satisfaction.begin(),satisfaction.end());
+
+        int n = satisfaction.size();
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // return solve(satisfaction,0,0,dp);
+
+        return solve(satisfaction);
+        
+    }
